cf5fb348e0f7cba0a7c7d2ff867593179b7d9edb
Akrishnaveni <annakrishnaveni05@gmail.com>
2023-09-06 05:38:21 +0000

    solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+const NotFound = () => (
+  <>
+    <div className="notfound-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/not-found-img.png "
+        alt="not found"
+        className="not-found-image"
+      />
+      <h3 className="heading">Lost your Way</h3>
+      <p className="not-found-description">
+        Sorry, we cannot find that page. You will find lots to explore on the
+        home page
+      </p>
+    </div>
+  </>
+)
+export default NotFound
